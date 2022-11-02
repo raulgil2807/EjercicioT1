6a4e183f8a947802c4b64a38b08e1b023b0d85df
raulgil2807 <115884465+raulgil2807@users.noreply.github.com>
2022-11-02 23:20:33 +0100

    Add files via upload

---
+++ ejercicio1/Ejercicio1.java
@@ -0,0 +1,39 @@
+package com.ejercicio1;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        //1 - Datos Numéricos
+
+        // 1.1 Tipo de datos: Enteros.
+
+        byte variable1 = 10;
+        short variable2 = 5;
+        int variable3 = 20;
+        long variable4 = 7;
+
+        System.out.println(variable1 + " " + variable2 + " " + variable3 + "" + " " + variable4);
+
+        // 1.2 Tipos de datos: Decimales.
+
+        float variable5 = 1.0f;
+        double variable6 = 10.4d;
+
+        System.out.println(variable5 + " " + variable6);
+
+        // 2. Tipos de datos: Booleanos.
+
+        boolean variable7 = true;
+        boolean variable8 = false;
+
+        System.out.println(variable7 + " " + variable8);
+
+        // 3. Tipos de datos: Texto.
+
+        char variable9 = 'a';
+        String variable10 = "Raul";
+
+        System.out.println(variable9 + " " + variable10);
+    }
+}
